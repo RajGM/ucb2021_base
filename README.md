e62d5d6f25768f5ccc3790d10f7bacbaa0183e4f
brookestevens <brst4163@colorado.edu>
2021-06-22 12:56:57 -0600

    initial theme commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ucb2021_base
-Drupal 9 Base theme for CU Boulder web properties 
+Drupal 9 Base theme for CU Boulder web properties
+
+## Linitng
+
+We are using stylelint and eslint
+
+````
+npm install          # install linters
+npm run stylelint    # lint css
+npm run eslint       # lint js
+````
---
+++ js/ucb-main.js
@@ -0,0 +1,13 @@
+/**************************************************
+ *
+ *   Main JS functionality for all pages.
+ *
+**************************************************/
+
+jQuery(document).ready(function () {
+  // handler to close the local tasks (edit, revisions, clone, etc...)
+  jQuery("a#ucbCloseLocalTasks").click(function (e) {
+    e.preventDefault();
+    jQuery(".ucb-local-tasks").hide();
+  });
+});
