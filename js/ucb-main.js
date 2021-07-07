8ab0888bac08276dcca5ac25c155ab4d686571f0
brookestevens <brst4163@colorado.edu>
2021-07-07 14:25:59 -0600

    create responsive main menu

--- js/ucb-main.js
+++ js/ucb-main.js
@@ -4,10 +4,20 @@
  *
 **************************************************/
 
-jQuery(document).ready(function () {
-  // handler to close the local tasks (edit, revisions, clone, etc...)
-  jQuery("a#ucbCloseLocalTasks").click(function (e) {
-    e.preventDefault();
-    jQuery(".ucb-local-tasks").hide();
+( $ => {
+  $(document).ready(() => {
+    // handler to close the local tasks (edit, revisions, clone, etc...)
+    $("a#ucbCloseLocalTasks").click(e => {
+      e.preventDefault();
+      $(".ucb-local-tasks").hide();
+    });
+
+    // Handle the responsive menu
+    $("#mobile-toggle").click(e => {
+      const menuHeight = $('.ucb-main-menu.nav').height() + $('.ucb-menu.nav').height() + "px";
+      // set a CSS variable to the menu height, which is dynamic
+      $("body").css("--menu-height", menuHeight);
+      $("section.ucb-main-nav-section").toggleClass('toggle');
+    });
   });
-});
+})(jQuery);
