ac012a1f1e2cc83fedefd94aaa5c5d3072746980
brookestevens <brst4163@colorado.edu>
2021-07-12 15:56:01 -0600

    linting

--- js/ucb-main.js
+++ js/ucb-main.js
@@ -13,11 +13,11 @@
     });
 
     // Handle the responsive menu
-    $("#mobile-toggle").click(e => {
-      const menuHeight = $('.ucb-main-menu.nav').height() + $('.ucb-menu.nav').height() + "px";
+    $("#mobile-toggle").click(() => {
+      const menuHeight = $(".ucb-main-menu.nav").height() + $(".ucb-menu.nav").height() + "px";
       // set a CSS variable to the menu height, which is dynamic
       $("body").css("--menu-height", menuHeight);
-      $("section.ucb-main-nav-section").toggleClass('toggle');
+      $("section.ucb-main-nav-section").toggleClass("toggle");
     });
   });
 })(jQuery);
