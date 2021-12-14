5026c21dffd76cb311b3993847cad028992cddde
jnicholCU <joshua.nicholson@colorado.edu>
2021-12-14 08:48:43 -0700

    Grid Layout yml, twig, js
    Added files and code for the content grid and column grid layout.
    Content Grid is the overarching page where all layouts will be chosen from.

---
+++ js/ucb-equalize-height.js
@@ -0,0 +1,33 @@
+function setHeight(el, val) {
+    if (typeof val === "function") val = val();
+    if (typeof val === "string") el.style.height = val;
+    else el.style.height = val + "px";
+  }
+  
+  var equalheight = function(container){
+    var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = new Array(),
+        $el,
+        topPosition = 0;
+  
+    Array.from(document.querySelectorAll(container)).forEach((el,i) => {
+      el.style.height = "auto";
+      topPostion = el.offsetTop;
+      if(currentRowStart != topPostion){
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+          setHeight(rowDivs[currentDiv], currentTallest)
+        }
+        rowDivs.length = 0;
+        currentRowStart = topPostion;
+        currentTallest = parseFloat(getComputedStyle(el, null).height.replace("px", ""))
+        rowDivs.push(el);
+      } else {
+        rowDivs.push(el);
+        currentTallest = (currentTallest < parseFloat(getComputedStyle(el, null).height.replace("px", ""))) ? (parseFloat(getComputedStyle(el, null).height.replace("px", ""))) : (currentTallest);
+      }
+      for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+        setHeight(rowDivs[currentDiv], currentTallest)
+      }
+    })
+  }
