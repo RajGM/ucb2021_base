f593b77684a65d0f44b1e80c0db543d49587d1f9
jnicholCU <joshua.nicholson@colorado.edu>
2021-12-14 14:42:45 -0700

    Update ucb-equalize-height.js
    eslint-disable-line, remove no-use-before-define

--- js/ucb-equalize-height.js
+++ js/ucb-equalize-height.js
@@ -1,19 +1,18 @@
-/*jslint unparam: true*/
 function setHeight(el, val) {
   if (typeof val === "function") val = val();
   if (typeof val === "string") el.style.height = val;
   else el.style.height = val + "px";
 }
 
-var equalheight = function (container) { // eslint-disable-line no-use-before-define
+var equalheight = function (container) { // eslint-disable-line
   var currentTallest = 0,
     currentRowStart = 0,
     rowDivs = new Array(),
-    $el, // eslint-disable-line no-use-before-define
+    $el, // eslint-disable-line
     topPosition = 0,
     currentDiv = 0;
 
-  Array.from(document.querySelectorAll(container)).forEach((el, i) => { // eslint-disable-line no-use-before-define
+  Array.from(document.querySelectorAll(container)).forEach((el, i) => { // eslint-disable-line
     el.style.height = "auto";
     topPosition = el.offsetTop;
     if (currentRowStart != topPosition) {
@@ -39,4 +38,3 @@ var equalheight = function (container) { // eslint-disable-line no-use-before-de
     }
   });
 };
-/*jslint unparam: false*/
